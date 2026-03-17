7cd797fbc9e75dce155e662a322587dd6638413d
maxicoleur <maximocoleur@gmail.com>
2026-03-17 20:04:19 -0300

    Update change.py

--- change.py
+++ change.py
@@ -3,3 +3,19 @@ def change():
     y lo separa en pesos (parte entera) y centavos.
     """
     pass
+
+    gasto=float(input("Ingrese su gasto: "))
+    dinero_recibido=float(input("Dinero recibido: "))
+
+
+
+    vuelto=dinero_recibido- gasto
+
+    parte_entera=int(vuelto)
+    centavos=int(100*(vuelto-int(vuelto)))
+
+    print("\nVuelto\n")
+
+    print(f"Pesos: \n {parte_entera}")
+
+    print(f"Centavos:\n {centavos}")
