62625aa2199d08d74e374e7dbb42b19261e39993
maxicoleur <maximocoleur@gmail.com>
2026-03-17 20:04:39 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -3,3 +3,13 @@ def check_vowels():
     sin distinguir mayúsculas de minúsculas.
     """
     pass
+
+    nombre=(input("Nombre: "))
+    name=nombre.lower()
+
+    print("Contiene a: ","a"in name)
+    print("Contiene e: ","e"in name)
+    print("Contiene i: ","i"in name)
+    print("Contiene o: ","o"in name)
+    print("Contiene u: ","u"in name)
+
